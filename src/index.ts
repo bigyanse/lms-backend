3e87bce8358a8b8184625e73b1c3febb00afb5d9
bigyanse <contact.bigyanse@gmail.com>
2023-05-22 15:38:45 +0545

    `update`: cors orign

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://coruscating-concha-8c7ccf.netlify.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
